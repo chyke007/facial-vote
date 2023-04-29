24710490f59e5e0052899b97aa8ef63deb6cb9d8
Chibuike Nwachukwu <chibuike.nwachukwu@payrix.com>
2023-04-29 18:18:37 +0100

    Implements admin auth for secured routes

--- facial-vote-backend/src/functions/cognito/create-user.js
+++ facial-vote-backend/src/functions/cognito/create-user.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 const { setErrorResponse, setSuccessResponse } = require('../../utils/helper');
-const { AWS_REGION, ADMIN_ACCESS_KEY, ADMIN_SECRET_KEY, USERPOOL_ID } = process.env
+const { AWS_REGION, ADMIN_ACCESS_KEY, ADMIN_SECRET_KEY, USERPOOL_ID, ADMIN_EMAIL } = process.env
 
 var Chance = require('chance');
 var chance = new Chance();
@@ -8,7 +8,12 @@ var chance = new Chance();
 const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({ region: AWS_REGION, accessKeyId: ADMIN_ACCESS_KEY, secretAccessKey: ADMIN_SECRET_KEY });
 
 module.exports.handler = async (event, context, callback) => {
-   
+    const admin = event?.requestContext?.authorizer?.claims?.email || null;
+
+    if (String(admin) != ADMIN_EMAIL) {
+        return setErrorResponse('Forbidden', 403)
+    }
+
     const { email } = JSON.parse(event.body);
 
     if (!email) {
@@ -16,8 +21,8 @@ module.exports.handler = async (event, context, callback) => {
     }
 
     var params = {
-        UserPoolId: USERPOOL_ID ,
-        Username: email, 
+        UserPoolId: USERPOOL_ID,
+        Username: email,
         DesiredDeliveryMediums: [
             'EMAIL'
         ],
@@ -35,12 +40,12 @@ module.exports.handler = async (event, context, callback) => {
             }
         ]
     };
-    
-   try{
-    await cognitoIdentityServiceProvider.adminCreateUser(
-        params
-      ).promise();
-      return setSuccessResponse("User created")
+
+    try {
+        await cognitoIdentityServiceProvider.adminCreateUser(
+            params
+        ).promise();
+        return setSuccessResponse("User created")
     } catch (err) {
         return setErrorResponse(err)
     }
--- facial-vote-backend/src/functions/cognito/get-users.js
+++ facial-vote-backend/src/functions/cognito/get-users.js
@@ -1,15 +1,20 @@
 const AWS = require('aws-sdk');
 const { setErrorResponse, setSuccessResponse } = require('../../utils/helper');
-const { AWS_REGION, ADMIN_ACCESS_KEY, ADMIN_SECRET_KEY, USERPOOL_ID } = process.env
+const { AWS_REGION, ADMIN_ACCESS_KEY, ADMIN_SECRET_KEY, USERPOOL_ID, ADMIN_EMAIL } = process.env
 
 const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({ region: AWS_REGION, accessKeyId: ADMIN_ACCESS_KEY, secretAccessKey: ADMIN_SECRET_KEY });
 
 module.exports.handler = async (event, context, callback) => {
-   
-   try{
-    let response = await cognitoIdentityServiceProvider.listUsers({ UserPoolId: USERPOOL_ID}).promise();
-      return setSuccessResponse(response.Users)
-    } catch (err) {
-        return setErrorResponse(err)
-    }
+  const admin = event?.requestContext?.authorizer?.claims?.email || null;
+
+  if (String(admin) != ADMIN_EMAIL) {
+    return setErrorResponse('Forbidden', 403)
+  }
+
+  try {
+    let response = await cognitoIdentityServiceProvider.listUsers({ UserPoolId: USERPOOL_ID }).promise();
+    return setSuccessResponse(response.Users)
+  } catch (err) {
+    return setErrorResponse(err)
+  }
 };
